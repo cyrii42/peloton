a79b25daade9a8ed601945222c08e1c5ca6efac4
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-09-18 09:39:31 -0400

    went back to functions from methods

--- peloton-to-mariadb.py
+++ peloton-to-mariadb.py
@@ -1,33 +1,27 @@
-## Potential useful questions to answer
-# how long since last workout?
-# how many workouts per week over last X weeks?
-# average calorie burn (or whatever) over last X rides?
-
 import pandas as pd
 from config import mariadb_conn
-from PylotonDataset import PylotonDataset
+import zmv_pyloton
 
 # Create Pandas DataFrame from existing table
 with mariadb_conn as conn:
-    db_dataframe = pd.read_sql("SELECT * from peloton", conn, index_col='start_time_iso', parse_dates=['start_time_iso', 'start_time_local'])
+    mariadb_df = pd.read_sql("SELECT * from peloton", conn, index_col='start_time_iso', parse_dates=['start_time_iso', 'start_time_local'])
 
-# Instantiate new PylotonDataset object 
-pyloton_dataset = PylotonDataset(db_dataframe)
-
-# Retrieve new workouts (if any)
-new_entries = pyloton_dataset.new_entries
+# Use MariaDB data to calculate number of new Peloton workouts 
+new_workouts_num = zmv_pyloton.calculate_new_workouts_num(mariadb_df)
 
 # If there are new entries:
-#   (i)    append DataFrame to MariaDB table
-#   (ii)   create new, concatenated "all_entries" DataFrame;
-#   (iii)  calculate new metrics and append to "all_entries" DataFrame; and
-#   (iv)   write Excel file
-if new_entries.shape[0] > 0:
+if new_workouts_num > 0:
+    # (1) retrieve new workout data
+    new_entries = zmv_pyloton.get_new_workouts(new_workouts_num)
+    
+    # (2) append DataFrame to MariaDB table
     with mariadb_conn as conn:
         new_entries.to_sql("peloton", conn, if_exists="append", index=False)
         
-    all_entries = pd.concat([db_dataframe, new_entries], ignore_index=True)
+    # (3) create new, concatenated "all_entries" DataFrame
+    all_entries = pd.concat([mariadb_df, new_entries], ignore_index=True)
 
+    # (4) calculate new metrics and append them to "all_entries" DataFrame
     duration_list = all_entries['duration'].tolist()
     length_list = all_entries['length'].tolist()
     output_list = all_entries['output'].tolist()
@@ -35,28 +29,13 @@ if new_entries.shape[0] > 0:
     all_entries['length_min'] = [(x / 60) for x in length_list]
     all_entries['output_per_min'] = [(x[0] / (x[1] / 60)) for x in zip(output_list, duration_list)]
 
+    # (5) write Excel file
     with pd.ExcelWriter('/mnt/home-ds920/peloton_workouts.xlsx', mode='a', if_sheet_exists='replace') as writer:
         all_entries.to_excel(writer, sheet_name='peloton_workouts', index=False, float_format='%.2f')
 
-# Print the final DataFrame
-if new_entries.shape[0] > 0:
+
+# OPTIONAL FOR TESTING: Print the final DataFrame
+if new_workouts_num > 0:
     print(new_entries)
 else:
-    print(db_dataframe)
-
-
-
-
-
-
-# if new_entries.shape[0] > 0:
-#     all_entries = pd.concat([db_dataframe, new_entries], ignore_index=True) 
-# else:
-#     all_entries = db_dataframe
-#     print(all_entries.tail)
-
-
-
-# start_datetime = datetime.fromtimestamp(start_timestamp, tz=eastern_time)
-# peloton_dict['start_time'].append(start_datetime)
-# start_iso = start_datetime.isoformat(sep='T')
+    print(mariadb_df)
