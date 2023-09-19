4bbfc8574a6b5cb1531b2f0188fe13ee2fe0a49c
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-09-19 12:22:35 -0400

    added NaiveDatetimeError exception class

--- config.py
+++ config.py
@@ -9,16 +9,27 @@ load_dotenv()
 # Time zone setup & UTC offset functions
 eastern_time = ZoneInfo("America/New_York")
 
-def utc_offset_int(ts: datetime=datetime.now()) -> int:
-    return int((ts.utcoffset().seconds / 60 / 60) - 24)
+class NaiveDatetimeError(Exception):
+    def __init__(self, dt, message="Input datetime object is not timezone-aware"):
+        self.dt = dt
+        self.message = message
+        super().__init__(self.message)
 
-def utc_offset_str(ts: datetime=datetime.now()) -> str:
-    utc_offset_int = int((ts.utcoffset().seconds / 60 / 60) - 24)
-    if utc_offset_int >= 0:
-        return f"0{utc_offset_int}:00"
-    else:
-        return f"-0{abs(utc_offset_int)}:00"
+def utc_offset_int(dt: datetime=datetime.now()) -> int:
+    if dt.utcoffset() != None:
+        return int((dt.utcoffset().seconds / 60 / 60) - 24)
+    else: 
+        raise NaiveDatetimeError(dt)
 
+def utc_offset_str(dt: datetime=datetime.now()) -> str:
+    if dt.utcoffset() != None:
+        utc_offset_int = int((dt.utcoffset().seconds / 60 / 60) - 24)
+        if utc_offset_int >= 0:
+            return f"0{utc_offset_int}:00"
+        else:
+            return f"-0{abs(utc_offset_int)}:00"
+    else: 
+        raise NaiveDatetimeError(dt)
 # InfluxDB setup
 influx_url = "http://mac-mini.box:8086"
 influx_org = "ZMV"
