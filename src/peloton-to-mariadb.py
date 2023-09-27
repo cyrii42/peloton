423b4df0a09170ef2aab8b9bf9e10b0fad114dc3
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-09-27 15:20:05 -0400

    added pivot tables to console output

--- src/peloton-to-mariadb.py
+++ src/peloton-to-mariadb.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from utils.pyloton_zmv import ingest_sql_data, calculate_new_workouts_num, \
     get_new_workouts, export_to_sql, calculate_excel_metrics
+from utils.peloton_pivots import get_pivot_table_year, get_pivot_table_month
 
 excel_filename = "/mnt/home-ds920/peloton_workouts.xlsx"
 
@@ -12,23 +13,26 @@ if __name__ == "__main__":
     # Use MariaDB data to calculate number of new Peloton workouts
     new_workouts_num = calculate_new_workouts_num(mariadb_df)
 
-    # If there are new entries:
+    # If there are new workouts, retrieve the data, write to MariaDB, 
+    #   re-pull from MariaDB, calculate new metrics, and write to Excel
     if new_workouts_num > 0:
-        # (1) retrieve new workout data
         new_entries = get_new_workouts(new_workouts_num)
         
-        # (2) append DataFrame to MariaDB table
         export_to_sql(new_entries)
             
-        # (3) Pull MariaDB data again, this time to create a new "all_entries" DataFrame
         all_entries = ingest_sql_data()
 
-        # (4) calculate new metrics and append them to "all_entries" DataFrame
         excel_df = calculate_excel_metrics(all_entries)
 
-        # (5) write Excel file
         with pd.ExcelWriter(excel_filename, mode='a', if_sheet_exists='replace') as writer:
             excel_df.to_excel(writer, sheet_name='peloton_workouts', index=False, float_format='%.2f') 
             
-        # (6) print the new DataFrame to console
+        print("New workout data:")
         print(all_entries)
+            
+    year_table = get_pivot_table_year()
+    month_table = get_pivot_table_month()
+
+    print(year_table.round(2))
+    print()
+    print(month_table.drop(columns=["annual_periods", "monthly_periods"]).round(2))
