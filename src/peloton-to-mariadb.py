46528da1b792759c635ec22d1bf29ed39d739094
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-09-27 23:02:54 -0400

    moved script to main() function

--- src/peloton-to-mariadb.py
+++ src/peloton-to-mariadb.py
@@ -3,9 +3,8 @@ from utils.pyloton_zmv import ingest_sql_data, calculate_new_workouts_num, \
     get_new_workouts, export_to_sql, calculate_excel_metrics
 from utils.peloton_pivots import get_pivot_table_year, get_pivot_table_month
 
-excel_filename = "/mnt/home-ds920/peloton_workouts.xlsx"
-
-if __name__ == "__main__":
+def main():
+    excel_filename = "/mnt/home-ds920/peloton_workouts.xlsx"
 
     # Pull MariaDB data and use it to calculate number of new Peloton workouts 
     mariadb_df = ingest_sql_data()
@@ -13,7 +12,7 @@ if __name__ == "__main__":
     # Use MariaDB data to calculate number of new Peloton workouts
     new_workouts_num = calculate_new_workouts_num(mariadb_df)
 
-    # If there are new workouts, retrieve the data, write to MariaDB, 
+    # If there are new workouts: retrieve the data, write to MariaDB, 
     #   re-pull from MariaDB, calculate new metrics, and write to Excel
     if new_workouts_num > 0:
         new_entries = get_new_workouts(new_workouts_num)
@@ -36,3 +35,7 @@ if __name__ == "__main__":
     print(year_table.round(2))
     print()
     print(month_table.drop(columns=["annual_periods", "monthly_periods"]).round(2))
+    
+    
+if __name__ == "__main__":
+    main()
