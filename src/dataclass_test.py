61ae535331f3c9526aa176b99a9f74e6789bb7c9
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-10-01 19:10:27 -0400

    almost there...

--- src/dataclass_test.py
+++ src/dataclass_test.py
@@ -5,57 +5,27 @@
 # GetFollowersById(self, userid=None)
 
 import pandas as pd
+from dataclasses import dataclass
+from typing import List
+from datetime import datetime, timezone
+from pylotoncycle import pylotoncycle
+from zoneinfo import ZoneInfo
+from utils.constants import EASTERN_TIME, PELOTON_USERNAME, PELOTON_PASSWORD
+from utils.helpers import create_mariadb_engine, get_peloton_data_from_sql, export_peloton_data_to_sql
 from peloton_to_mariadb import calculate_new_workouts_num, get_new_workouts
 from utils.peloton_pivots import get_sql_data_for_pivots, get_pivot_table_year, get_pivot_table_month
-from dataclasses import dataclass, fields
-from typing import List
-from datetime import datetime
-import json
-from pylotoncycle import pylotoncycle
-from utils.constants import PELOTON_USERNAME, PELOTON_PASSWORD
-from utils.helpers import create_mariadb_engine, get_peloton_data_from_sql, export_peloton_data_to_sql
-# from decimal import Decimal, getcontext
-
-# getcontext().prec = 4
-
-# print(getcontext())
-# print(Decimal('3434832.417') + Decimal(0))
-
-# @dataclass
-# class DCTest:
-#     name: str = None
-#     value: Decimal = None 
-#     value2: Decimal = None
-    
-# dc_test1 = DCTest('bobby jones', 42)
-# dc_test2 = DCTest('billy ray', 42.524, 99)
-# dc_test3 = DCTest()
-# dc_test3.name = 'betty sue'
-# dc_test3.value = 5582.34
-
-# print(dc_test1)
-# print(dc_test2)
-# print(dc_test1.value + dc_test2.value)
-# print(type(dc_test2.value))
-# print(dc_test3)
-    
-
-# Create PylotonCycle connection object
-py_conn = pylotoncycle.PylotonCycle(PELOTON_USERNAME, PELOTON_PASSWORD)
-
-
-
-    
-
+ 
 @dataclass
 class PelotonRide:
     id: str = None
-    ride_title: str = None
-    instructor: str = None
-    ride_description: str = None
-    created_at: int = None
     start_time: int = None
+    start_time_iso: str = None
     end_time: int = None
+    end_time_iso: str = None
+    ride_title: str = None
+    instructor_name: str = None
+    ride_instructor_id: str = None
+    ride_description: str = None
     user_id: str = None
     ride_id: str = None
     ride_image_url: str = None
@@ -82,56 +52,55 @@ class PelotonRide:
     resistance_max: int = None
     speed_avg: float = None
     speed_max: float = None
-    hr_avg: int = None
-    hr_max: int = None
+    heart_rate_avg: int = None
+    heart_rate_max: int = None
     strive_score: float = None
-    hr_zone1_duration: int = None
-    hr_zone2_duration: int = None
-    hr_zone3_duration: int = None
-    hr_zone4_duration: int = None
+    heart_rate_z1_duration: int = None
+    heart_rate_z2_duration: int = None
+    heart_rate_z3_duration: int = None
+    heart_rate_z4_duration: int = None
+    
+    def set_datetimes(self):
+        if self.start_time:          
+            if self.timezone:
+                self.start_time_iso = datetime.fromtimestamp(self.start_time, tz=ZoneInfo(self.timezone)).isoformat()
+            else:
+                self.start_time_iso = datetime.fromtimestamp(
+                    self.start_time, tz=EASTERN_TIME).isoformat()
+        if self.end_time:          
+            if self.timezone:
+                self.end_time_iso = datetime.fromtimestamp(self.end_time, tz=ZoneInfo(self.timezone)).isoformat()
+            else:
+                self.end_time_iso = datetime.fromtimestamp(
+                    self.end_time, tz=EASTERN_TIME).isoformat()     
+                
+                
+    # def create_dataframe_for_sql(self):
+        
+            
     
     
 @dataclass
 class RideGroup:
     rides: List[PelotonRide]
-
-
-##### This might be a nice upgrade, but consider moving everything to a Dataclass.  Then you
-##### might be able to specify default "None" values for some of these things, which means you could
-##### dispense with all the checking.
-
-# Check if specified column (or alternate column) exists in DataFrame; if so, return full column (as a Series)
-def check_column(column: str, df: pd.DataFrame, alternate_column: str=None):
-    if column in df.columns:
-        return df[column]
-    elif alternate_column in df.columns:
-        return df[alternate_column]
-    else:
-        if alternate_column == None:
-            print(f"Columnn {column} does not exist!")
-        else:
-            print(f"Neither column {column} nor column {alternate_column} exists!")
-        return None
     
-    
-# Check if specified row number exists; if so, return the value of that row in specified column
-def check_row(row: int, column: str, df: pd.DataFrame) -> pd.Series | None:
-    if row < df.shape[0]:
-        return df[column][row]
-    else:
-        print(f"Row #{row} does not exist!")
-        return None
+    def __str__(self):
+        string = ""
+        for ride in self.rides:
+            string = string + "\n" + "\n" + ride.__repr__()
+        return f"{self.__class__.__name__} ({len(self.rides)} rides):{string}"
 
 
-def loop_through_workouts(num: int=3):
-    workouts = py_conn.GetRecentWorkouts(num)
+
+def loop_through_workouts(py_conn: pylotoncycle.PylotonCycle, workouts_num: int):
+    workouts = py_conn.GetRecentWorkouts(workouts_num)
+    ride_objects_list = []
     for w in workouts:
         ride_object = PelotonRide()
         df_workout = pd.json_normalize(w)
         df_workout_ride = pd.json_normalize(w['ride'])
         workout_id = df_workout['id'].loc[0]
         
-
         # Loop through regular columns in df_workout
         for column in df_workout.columns:
             if not df_workout[column].dropna().empty:
@@ -142,65 +111,61 @@ def loop_through_workouts(num: int=3):
             if not df_workout_ride[column].dropna().empty:
                 setattr(ride_object, f"ride_{column}", df_workout_ride[column][0])
 
-
-    
+        # Pull metrics from Peloton, copy into a dictionary, create DataFrames
         workout_metrics_by_id_dict = py_conn.GetWorkoutMetricsById(workout_id)
-        
         df_summaries = pd.json_normalize(workout_metrics_by_id_dict['summaries'])
         df_metrics = pd.json_normalize(workout_metrics_by_id_dict['metrics'])
+        df_effort_zones = pd.json_normalize(workout_metrics_by_id_dict['effort_zones'])
+        df_hr_zone_durations = pd.json_normalize(workout_metrics_by_id_dict['effort_zones']['heart_rate_zone_durations'])
         df_average_values = df_metrics.set_index('slug')['average_value']
         df_max_values = df_metrics.set_index('slug')['max_value']
-
-        print(df_summaries)
-        print(df_metrics['average_value'])
-        print(df_metrics['max_value'])
         
+        # Loop through summaries (total_output, distance, calories)
         for index, row in df_summaries.iterrows():
             setattr(ride_object, df_summaries['slug'][index], df_summaries['value'][index])
 
+        # Loop through average values (output, cadence, resistance, speed, HR)
         for index, value in df_average_values.items():
             setattr(ride_object, f"{index}_avg", value)
-            
+          
+        # Loop through maximum values (output, cadence, resistance, speed, HR)  
         for index, value in df_max_values.items():
             setattr(ride_object, f"{index}_max", value)
+            
+        # Write "total_effort_points" to "scrive_score"
+        setattr(ride_object, "strive_score", df_effort_zones['total_effort_points'][0])
+        
+        # Loop through HR zone durations
+        for column in df_hr_zone_durations.columns:
+            if not df_hr_zone_durations[column].dropna().empty:
+                setattr(ride_object, column, df_hr_zone_durations[column][0])
 
-        
-        # df_summaries = pd.json_normalize(workout_metrics_by_id_dict['summaries'])
-        # for row in df_summaries:
-            
-        # if 'value' in df_summaries.columns:
-        
-        #     ride_object.output_total = check_row(0, 'value', df_summaries)
-        #     ride_object.distance = check_row(1, 'value', df_summaries)
-        #     ride_object.calories_total = check_row(2, 'value', df_summaries)
-        
-        # df_average_summaries = pd.json_normalize(workout_metrics_by_id_dict['average_summaries'])
-        # if 'max_value' in df_average_summaries.columns:
-        #     ride_object.output_avg = check_row(0, 'average_value', df_average_summaries)
-        #     ride_object.output_max = check_row(0, 'max_value', df_average_summaries)
-        #     ride_object.cadence_avg = check_row(1, 'average_value', df_average_summaries)
-        #     ride_object.cadence_max = check_row(1, 'max_value', df_average_summaries)
-        #     ride_object.resistance_avg = check_row(2, 'average_value', df_average_summaries)
-        #     ride_object.resistance_max = check_row(2, 'max_value', df_average_summaries)
-        #     ride_object.speed_avg = check_row(3, 'average_value', df_average_summaries)
-        #     ride_object.speed_max = check_row(3, 'max_value',df_average_summaries)
-        #     ride_object.hr_avg = check_row(4, 'average_value', df_average_summaries)
-        #     ride_object.hr_max = check_row(4, 'max_value', df_average_summaries)
-            
-        # if 'effort_zones' in workout_metrics_by_id_dict:
-        #     df_effort_zones = pd.json_normalize(workout_metrics_by_id_dict['effort_zones'])
-            
-        #     ride_object.strive_score = check_column('total_effort_points', df_effort_zones)[0]
-        #     ride_object.hr_zone1_duration = check_column('heart_rate_zone_durations.heart_rate_z1_duration', df_effort_zones)[0]
-        #     ride_object.hr_zone2_duration = check_column('heart_rate_zone_durations.heart_rate_z2_duration', df_effort_zones)[0]
-        #     ride_object.hr_zone3_duration = check_column('heart_rate_zone_durations.heart_rate_z3_duration', df_effort_zones)[0]
-        #     ride_object.hr_zone4_duration = check_column('heart_rate_zone_durations.heart_rate_z4_duration', df_effort_zones)[0]
-            
-        print(ride_object)
+        # Go back through and create ISO strings from "start_time" and "end_time"
+        ride_object.set_datetimes()
+
+        # We've pulled everything from this ride, so add this new ride object to the running list
+        ride_objects_list.append(ride_object)
+                                 
+    # Once we've looped through all new rides, create a RideGroup object from the list
+    new_rides = RideGroup(ride_objects_list)
+    print(new_rides)    
+    
+
+def main():
+    sql_engine = create_mariadb_engine(database="zmv")
+    
+    py_conn = pylotoncycle.PylotonCycle(PELOTON_USERNAME, PELOTON_PASSWORD) 
+    
+    loop_through_workouts(py_conn, workouts_num=3)   
         
         
 if __name__ == "__main__":
-    loop_through_workouts(1)
+    main()
+    
+    
+    
+    
+    
     
     # workouts = py_conn.GetWorkoutMetricsById("1549b39e75cb48c0ac6179b952ce2cac")
     # df = pd.json_normalize(workouts['metrics'])
@@ -244,6 +209,41 @@ if __name__ == "__main__":
 # # else:
 # #     print("noooooo")
 
+
+
+        
+        # df_summaries = pd.json_normalize(workout_metrics_by_id_dict['summaries'])
+        # for row in df_summaries:
+            
+        # if 'value' in df_summaries.columns:
+        
+        #     ride_object.output_total = check_row(0, 'value', df_summaries)
+        #     ride_object.distance = check_row(1, 'value', df_summaries)
+        #     ride_object.calories_total = check_row(2, 'value', df_summaries)
+        
+        # df_average_summaries = pd.json_normalize(workout_metrics_by_id_dict['average_summaries'])
+        # if 'max_value' in df_average_summaries.columns:
+        #     ride_object.output_avg = check_row(0, 'average_value', df_average_summaries)
+        #     ride_object.output_max = check_row(0, 'max_value', df_average_summaries)
+        #     ride_object.cadence_avg = check_row(1, 'average_value', df_average_summaries)
+        #     ride_object.cadence_max = check_row(1, 'max_value', df_average_summaries)
+        #     ride_object.resistance_avg = check_row(2, 'average_value', df_average_summaries)
+        #     ride_object.resistance_max = check_row(2, 'max_value', df_average_summaries)
+        #     ride_object.speed_avg = check_row(3, 'average_value', df_average_summaries)
+        #     ride_object.speed_max = check_row(3, 'max_value',df_average_summaries)
+        #     ride_object.hr_avg = check_row(4, 'average_value', df_average_summaries)
+        #     ride_object.hr_max = check_row(4, 'max_value', df_average_summaries)
+            
+        # if 'effort_zones' in workout_metrics_by_id_dict:
+        #     df_effort_zones = pd.json_normalize(workout_metrics_by_id_dict['effort_zones'])
+            
+        #     ride_object.strive_score = check_column('total_effort_points', df_effort_zones)[0]
+        #     ride_object.hr_zone1_duration = check_column('heart_rate_zone_durations.heart_rate_z1_duration', df_effort_zones)[0]
+        #     ride_object.hr_zone2_duration = check_column('heart_rate_zone_durations.heart_rate_z2_duration', df_effort_zones)[0]
+        #     ride_object.hr_zone3_duration = check_column('heart_rate_zone_durations.heart_rate_z3_duration', df_effort_zones)[0]
+        #     ride_object.hr_zone4_duration = check_column('heart_rate_zone_durations.heart_rate_z4_duration', df_effort_zones)[0]
+            
+        # print(ride_object)
 
 # # df.to_csv('/mnt/home-ds920/asdf9.csv')
         
@@ -320,3 +320,52 @@ if __name__ == "__main__":
 #     workout_id = df_workout['id'].loc[0]
 #     df_
 # '''
+
+# from decimal import Decimal, getcontext
+
+# getcontext().prec = 4
+
+# print(getcontext())
+# print(Decimal('3434832.417') + Decimal(0))
+
+# @dataclass
+# class DCTest:
+#     name: str = None
+#     value: Decimal = None 
+#     value2: Decimal = None
+    
+# dc_test1 = DCTest('bobby jones', 42)
+# dc_test2 = DCTest('billy ray', 42.524, 99)
+# dc_test3 = DCTest()
+# dc_test3.name = 'betty sue'
+# dc_test3.value = 5582.34
+
+# print(dc_test1)
+# print(dc_test2)
+# print(dc_test1.value + dc_test2.value)
+# print(type(dc_test2.value))
+# print(dc_test3)
+
+    
+
+# # Check if specified column (or alternate column) exists in DataFrame; if so, return full column (as a Series)
+# def check_column(column: str, df: pd.DataFrame, alternate_column: str=None):
+#     if column in df.columns:
+#         return df[column]
+#     elif alternate_column in df.columns:
+#         return df[alternate_column]
+#     else:
+#         if alternate_column == None:
+#             print(f"Columnn {column} does not exist!")
+#         else:
+#             print(f"Neither column {column} nor column {alternate_column} exists!")
+#         return None
+    
+    
+# # Check if specified row number exists; if so, return the value of that row in specified column
+# def check_row(row: int, column: str, df: pd.DataFrame) -> pd.Series | None:
+#     if row < df.shape[0]:
+#         return df[column][row]
+#     else:
+#         print(f"Row #{row} does not exist!")
+#         return None
