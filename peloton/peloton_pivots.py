3b207b44674da9187d0b0913ae989919ffd487e0
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-10-06 18:52:45 -0400

    fixed for new MariaDB schema; fixed datetime TZ

--- peloton/peloton_pivots.py
+++ peloton/peloton_pivots.py
@@ -4,40 +4,42 @@ from helpers import create_mariadb_engine
 
 def get_sql_data_for_pivots(engine: db.Engine) -> pd.DataFrame():
     with engine.connect() as conn:
-       df = pd.read_sql(f"SELECT * from peloton", conn, parse_dates=['start_time_iso', 'start_time_local'])
+        df = pd.read_sql_table("peloton", conn)
+
+    df['start_time_iso'] = pd.to_datetime(df['start_time_iso'], utc=True)    
 
     df['annual_periods'] = [x.to_period(freq='Y') for x in df['start_time_iso'].tolist()]
     df['monthly_periods'] = [x.to_period(freq='M') for x in df['start_time_iso'].tolist()]
     df['month_name'] = [x.month_name() + " " + str(x.year) for x in df['start_time_iso'].tolist()]
     # df['weekly_periods'] = [x.to_period(freq='W') for x in df['start_time_iso'].tolist()]
 
-    output_list = df['output'].tolist()
-    duration_list = df['duration'].tolist()
-    df['output_per_min'] = [(x[0] / (x[1] / 60)) for x in zip(output_list, duration_list)]
-    df['duration_min'] = [int(round((x / 60),0)) for x in duration_list]
-    df['unique_days'] = [x.date() for x in df['start_time_local'].tolist()]
+    output_list = df['total_output'].tolist()
+    duration_list = df['ride_duration'].tolist()
+    df['output_per_min'] = [(x[0] / (x[1] / 60)) for x in zip(output_list, duration_list) if x[1] != 0]
+    df['duration_min'] = [int(round((x / 60), 0)) for x in duration_list if x != 0]
+    df['unique_days'] = [x.date() for x in df['start_time_iso'].tolist()]
     
     return df.reset_index()
 
 def get_pivot_table_year(df: pd.DataFrame) -> pd.DataFrame():
     year_table = df.pivot_table(
         values=[
-            'title', 
+            'ride_title', 
             'unique_days',
             'duration_min',
             'calories',
             'distance',
-            'difficulty',
+            'ride_difficulty_estimate',
             'output_per_min'
             ], 
         index=['annual_periods'],
         aggfunc= {
-            'title': 'count', 
+            'ride_title': 'count', 
             'unique_days': pd.Series.nunique, 
             'duration_min': 'sum', 
             'calories': 'mean', 
             'distance': 'sum', 
-            'difficulty': 'mean', 
+            'ride_difficulty_estimate': 'mean', 
             'output_per_min': 'mean'
             }
         )
@@ -47,12 +49,12 @@ def get_pivot_table_year(df: pd.DataFrame) -> pd.DataFrame():
 def get_pivot_table_month(df: pd.DataFrame) -> pd.DataFrame():
     month_table = df.pivot_table( 
         values=[
-            'title', 
+            'ride_title', 
             'unique_days',
             'duration_min',
             'calories',
             'distance',
-            'difficulty',
+            'ride_difficulty_estimate',
             'output_per_min'
             ], 
         index=[
@@ -61,12 +63,12 @@ def get_pivot_table_month(df: pd.DataFrame) -> pd.DataFrame():
             'month_name'
             ], 
         aggfunc= {
-            'title': 'count', 
+            'ride_title': 'count', 
             'unique_days': pd.Series.nunique, 
             'duration_min': 'sum', 
             'calories': 'mean', 
             'distance': 'sum', 
-            'difficulty': 'mean', 
+            'ride_difficulty_estimate': 'mean', 
             'output_per_min': 'mean'
             }
         )
@@ -75,7 +77,7 @@ def get_pivot_table_month(df: pd.DataFrame) -> pd.DataFrame():
 
 
 def main():
-    mariadb_engine = create_mariadb_engine(database="zmv")
+    mariadb_engine = create_mariadb_engine(database="peloton")
     
     df = get_sql_data_for_pivots(mariadb_engine)
     
