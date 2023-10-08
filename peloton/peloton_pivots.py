710797a91cabf3b6862df84c24f542cacf7f9aec
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-10-07 20:09:39 -0400

    fixed period issues, renamed & reordered columns

--- peloton/peloton_pivots.py
+++ peloton/peloton_pivots.py
@@ -9,73 +9,103 @@ def get_sql_data_for_pivots(engine: db.Engine) -> pd.DataFrame():
         df = pd.read_sql_table("peloton", conn)
 
     df['start_time_iso'] = pd.to_datetime(df['start_time_iso'], utc=True)    
+    df_dti = pd.DatetimeIndex(df['start_time_iso']).tz_localize(tz=None)
 
-    df['annual_periods'] = [x.to_period(freq='Y') for x in df['start_time_iso'].tolist()]
-    df['monthly_periods'] = [x.to_period(freq='M') for x in df['start_time_iso'].tolist()]
-    df['month_name'] = [x.month_name() + " " + str(x.year) for x in df['start_time_iso'].tolist()]
-    # df['weekly_periods'] = [x.to_period(freq='W') for x in df['start_time_iso'].tolist()]
+    df['annual_periods'] = [x.to_period(freq='Y') for x in df_dti]
+    df['monthly_periods'] = [x.to_period(freq='M') for x in df_dti]
+    df['month'] = [x.month_name() + " " + str(x.year) for x in df_dti]
+    df['year'] = [x.year for x in df_dti]
+    # df['weekly_periods'] = [x.to_period(freq='W') for x in df_dti]
 
     output_list = df['total_output'].tolist()
     duration_list = df['ride_duration'].tolist()
     df['output_per_min'] = [(x[0] / (x[1] / 60)) for x in zip(output_list, duration_list) if x[1] != 0]
     df['duration_min'] = [int(round((x / 60), 0)) for x in duration_list if x != 0]
     df['unique_days'] = [x.date() for x in df['start_time_iso'].tolist()]
+
+    df = df.rename(columns={
+        'ride_title': 'title',
+        'duration_min': 'min', 
+        'unique_days': 'days',
+        'ride_difficulty_estimate': 'difficulty',
+        'output_per_min': 'output/min',
+        })
     
     return df.reset_index()
+
 
 def get_pivot_table_year(df: pd.DataFrame) -> pd.DataFrame():
     year_table = df.pivot_table(
         values=[
-            'ride_title', 
-            'unique_days',
-            'duration_min',
+            'title', 
+            'days',
+            'min',
             'calories',
             'distance',
-            'ride_difficulty_estimate',
-            'output_per_min'
+            'difficulty',
+            'output/min'
             ], 
-        index=['annual_periods'],
+        index=['annual_periods', 'year'],
         aggfunc= {
-            'ride_title': 'count', 
-            'unique_days': pd.Series.nunique, 
-            'duration_min': 'sum', 
+            'title': 'count', 
+            'days': pd.Series.nunique, 
+            'min': 'sum', 
             'calories': 'mean', 
             'distance': 'sum', 
-            'ride_difficulty_estimate': 'mean', 
-            'output_per_min': 'mean'
+            'difficulty': 'mean', 
+            'output/min': 'mean'
             }
         )
+
+    year_table = year_table.sort_values(by=['annual_periods'], ascending=False)
+    year_table = year_table.reset_index().round(2)
+    year_table = year_table.drop(columns=['annual_periods'])
+    year_table = year_table.rename(columns={
+        'title': 'rides'
+    })
+    # Change the column order
+    year_table = year_table.reindex(columns=['year', 'rides', 'days', 'calories', 'distance', 'difficulty', 'output/min'])
     
-    return year_table.reset_index().round(2)
+    return year_table
+
 
 def get_pivot_table_month(df: pd.DataFrame) -> pd.DataFrame():
     month_table = df.pivot_table( 
         values=[
-            'ride_title', 
-            'unique_days',
-            'duration_min',
+            'title', 
+            'days',
+            'min',
             'calories',
             'distance',
-            'ride_difficulty_estimate',
-            'output_per_min'
+            'difficulty',
+            'output/min'
             ], 
         index=[
             'annual_periods', 
             'monthly_periods', 
-            'month_name'
+            'month'
             ], 
         aggfunc= {
-            'ride_title': 'count', 
-            'unique_days': pd.Series.nunique, 
-            'duration_min': 'sum', 
+            'title': 'count', 
+            'days': pd.Series.nunique, 
+            'min': 'sum', 
             'calories': 'mean', 
             'distance': 'sum', 
-            'ride_difficulty_estimate': 'mean', 
-            'output_per_min': 'mean'
+            'difficulty': 'mean', 
+            'output/min': 'mean'
             }
         )
+
+    month_table = month_table.sort_values(by=['monthly_periods'], ascending=False)
+    month_table = month_table.reset_index().round(2)
+    month_table = month_table.drop(columns=['annual_periods', 'monthly_periods'])
+    month_table = month_table.rename(columns={
+        'title': 'rides'
+    })
+    # Change the column order
+    month_table = month_table.reindex(columns=['month', 'rides', 'days', 'calories', 'distance', 'difficulty', 'output/min'])
     
-    return month_table.reset_index().round(2)
+    return month_table
 
 
 def main():
