a98b89375ea56a17f8f31ddea871cdf432c9618f
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-10-09 14:47:01 -0400

    added select_all_from_table()

--- peloton/helpers.py
+++ peloton/helpers.py
@@ -21,6 +21,16 @@ def create_mariadb_engine(database: str) -> db.Engine:
     return db.create_engine(mariadb_url)
 
 
+def select_all_from_table(
+    engine: db.Engine, table: str, index_col: str = None, parse_dates: list[str] = None
+) -> pd.DataFrame:
+    with engine.connect() as conn:
+        df = pd.read_sql(
+            f"SELECT * from {table}", conn, index_col=index_col, parse_dates=parse_dates
+        )
+    return df
+
+
 # UTC offset functions
 class NaiveDatetimeError(Exception):
     def __init__(self, dt, message="Input datetime object is not timezone-aware"):
