f2046b8ba80a87a5ae8e59a5a28aa6ee1d66feab
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2023-10-18 12:40:18 -0400

    updated PelotonRide() parameters

--- peloton/classes.py
+++ peloton/classes.py
@@ -1,5 +1,5 @@
 import inspect
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 from typing import List
 from zoneinfo import ZoneInfo
@@ -12,33 +12,33 @@ from peloton.constants import EASTERN_TIME
 
 @dataclass
 class PelotonRide:
-    workout_id: str = None
-    start_time: int = None
-    start_time_iso: str = None
-    end_time: int = None
-    end_time_iso: str = None
-    ride_title: str = None
-    instructor_name: str = None
+    workout_id: str
+    timezone: str
+    start_time: int
+    start_time_iso: str = field(init=False)
+    end_time: int
+    end_time_iso: str = field(init=False)
+    ride_title: str
+    workout_type: str
+    metrics_type: str
+    user_id: str
+    distance: float
+    calories: float
+    total_output: float
+    instructor_name: str
+    total_work: float = None
     ride_instructor_id: str = None
     ride_description: str = None
-    user_id: str = None
+    ride_difficulty_estimate: float = None
     ride_id: str = None
     ride_image_url: str = None
-    timezone: str = None
-    metrics_type: str = None
-    total_work: float = None
     has_pedaling_metrics: str = None
     has_leaderboard_metrics: str = None
-    workout_type: str = None
     average_effort_score: float = None
     leaderboard_rank: int = None
     total_leaderboard_users: int = None
     ride_duration: int = None
     ride_length: int = None
-    distance: float = None
-    ride_difficulty_estimate: float = None
-    calories: float = None
-    total_output: float = None
     output_avg: int = None
     output_max: int = None
     cadence_avg: int = None
