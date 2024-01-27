32528381f989515a3d155d716245ca9bb4f70790
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-01-27 17:28:33 -0500

    moved SQL functions to here; added `create_pylotoncycle_conn()`

--- peloton/helpers.py
+++ peloton/helpers.py
@@ -5,20 +5,63 @@ from datetime import datetime
 
 import pandas as pd
 import sqlalchemy as db
+from pylotoncycle import PylotonCycle
 
-from peloton.constants import MARIADB_PASS, MARIADB_SERVER, MARIADB_USER
+import peloton.constants as const
 
+def create_pyltotoncycle_conn() -> PylotonCycle:
+    return PylotonCycle(const.PELOTON_USERNAME, const.PELOTON_PASSWORD) 
 
 # SQL database functions
 def create_mariadb_engine(database: str) -> db.Engine:
     mariadb_url = db.URL.create(
         drivername="mysql+pymysql",
-        username=MARIADB_USER,
-        password=MARIADB_PASS,
-        host=MARIADB_SERVER,
+        username=const.MARIADB_USER,
+        password=const.MARIADB_PASS,
+        host=const.MARIADB_SERVER,
         database=database,
     )
     return db.create_engine(mariadb_url)
+
+def export_raw_workout_data_to_sql(input_df: pd.DataFrame, engine: db.Engine):
+    # Convert all datatypes (other than int64/float64) to strings for subsequent SQL export
+    for column in input_df.select_dtypes(exclude=['int64', 'float64', 'bool']).columns:
+        input_df[column] = input_df[column].astype("string")
+
+    with engine.connect() as conn:
+        input_df.to_sql("raw_data_workouts", conn, if_exists="append", index=False)
+
+
+def export_raw_metrics_data_to_sql(input_df: pd.DataFrame, engine: db.Engine):
+    # Convert all datatypes (other than int64/float64) to strings for subsequent SQL export
+    for column in input_df.select_dtypes(exclude=['int64', 'float64', 'bool']).columns:
+        input_df[column] = input_df[column].astype("string")
+        
+    with engine.connect() as conn:
+        input_df.to_sql("raw_data_metrics", conn, if_exists="append", index=False)
+
+
+def export_processed_data_to_sql(input_df: pd.DataFrame, engine: db.Engine):
+    with engine.connect() as conn:
+        input_df.to_sql("peloton", conn, if_exists="append", index=False)
+
+
+def ingest_raw_workout_data_from_sql(engine: db.Engine) -> pd.DataFrame:
+    with engine.connect() as conn:
+        df = pd.read_sql("SELECT * from raw_data_workouts", conn)
+    return df
+
+
+def ingest_raw_metrics_data_from_sql(engine: db.Engine) -> pd.DataFrame:
+    with engine.connect() as conn:
+        df = pd.read_sql("SELECT * from raw_data_metrics", conn)
+    return df
+
+
+def ingest_processed_data_from_sql(engine: db.Engine) -> pd.DataFrame:
+    with engine.connect() as conn:
+        df = pd.read_sql("SELECT * from peloton", conn)
+    return df
 
 
 def select_all_from_table(
