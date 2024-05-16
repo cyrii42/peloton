5c83e0e0c46dd4aaa4a7822c8556e19b6d040f9a
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-05-16 15:41:34 -0400

    separated "summary" and "ride"; renamed interim DF variables from "dict_list" to "model_dump_list"

--- peloton/schema/workout_data.py
+++ peloton/schema/workout_data.py
@@ -58,24 +58,25 @@ class PelotonWorkoutData(BaseModel):
         # print(f"Making dataframe for workout {self.workout_id} at {datetime.now(tz=EASTERN_TIME)}...")
         output_dict = {}
         
-        summary_dict = self.summary.model_dump()
-        ride_dict = summary_dict.pop('ride')
+        summary_dict = self.summary.model_dump(exclude={'ride'})
         output_dict.update(summary_dict)
+        
+        ride_dict = self.summary.ride.model_dump()
         output_dict.update(ride_dict)
 
-        metrics_summaries_dict_list = [x.model_dump() for x in self.metrics.summaries]
-        metrics_summaries_dict_list = [{d['slug']: d['value']} for d in metrics_summaries_dict_list]
+        metrics_summaries_model_dump_list = [x.model_dump() for x in self.metrics.summaries]
+        metrics_summaries_dict_list = [{d['slug']: d['value']} for d in metrics_summaries_model_dump_list]
         combined_dict = {key: value for d in metrics_summaries_dict_list for key, value in d.items()}
         output_dict.update(combined_dict)
 
-        metrics_metrics_dict_list = [x.model_dump() for x in self.metrics.metrics]
-        metrics_metrics_dict_list = ([{f"avg_{d['slug']}": d['average_value']} for d in metrics_metrics_dict_list] 
-                                       + [{f"max_{d['slug']}": d['max_value']} for d in metrics_metrics_dict_list])
+        metrics_metrics_model_dump_list = [x.model_dump() for x in self.metrics.metrics]
+        metrics_metrics_dict_list = ([{f"avg_{d['slug']}": d['average_value']} for d in metrics_metrics_model_dump_list] 
+                                       + [{f"max_{d['slug']}": d['max_value']} for d in metrics_metrics_model_dump_list])
         combined_dict = {key: value for d in metrics_metrics_dict_list for key, value in d.items()}
         output_dict.update(combined_dict)
 
-        metrics_hr_zones_dict_list = [x.model_dump() for x in self.metrics.metrics]
-        metrics_hr_zones_dict_list = [d['zones'] for d in metrics_hr_zones_dict_list if d['zones'] is not None]
+        metrics_hr_zones_model_dump_list = [x.model_dump() for x in self.metrics.metrics]
+        metrics_hr_zones_dict_list = [d['zones'] for d in metrics_hr_zones_model_dump_list if d['zones'] is not None]
         if len(metrics_hr_zones_dict_list) > 0:
             metrics_hr_zones_dict_list = [{f"hr_{d['slug']}": d['duration']} for d in metrics_hr_zones_dict_list[0]]
             combined_dict = {key: value for d in metrics_hr_zones_dict_list for key, value in d.items()}
