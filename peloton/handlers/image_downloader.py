bf61150ba841a15d5eae41f8939713a88ec6050a
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-05-28 22:42:43 -0400

    switched to urllib3

--- peloton/handlers/image_downloader.py
+++ peloton/handlers/image_downloader.py
@@ -1,4 +1,4 @@
-from urllib.request import urlretrieve
+import urllib3
 
 from peloton.constants import IMAGES_DIR
 from peloton.schema import PelotonWorkoutData
@@ -10,10 +10,23 @@ class PelotonImageDownloader():
 
     @staticmethod
     def download_workout_image(workout: PelotonWorkoutData) -> None:
-        image_url = workout.summary.ride.image_url
-        local_filename = IMAGES_DIR.joinpath(image_url.split(sep='/')[-1])
+        if workout.summary.ride.image_url is not None:
+            print(f"Downloading image for workout {workout.workout_id}...")
 
-        urlretrieve(image_url, local_filename)
+            image_url = workout.summary.ride.image_url
+            local_filename = IMAGES_DIR.joinpath(image_url.split(sep='/')[-1])
+
+            http = urllib3.PoolManager()
+            try:
+                response: urllib3.response.BaseHTTPResponse = http.request('GET', image_url)           
+            except urllib3.exceptions.LocationValueError as e:
+                print(e)
+            else:
+                if response.status == 200:
+                    with open(local_filename, 'wb') as file:
+                        file.write(response.data)
+                else:
+                    print(f"ERROR - HTTP Status Code: {response.status}")
 
 
 def main():
