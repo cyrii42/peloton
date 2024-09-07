0ce621076fefdbeebdb823a02a458498f2110ad0
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-06 21:55:16 -0400

    moved `csv_handler` and `stdout_printer` to trash; removed imports

--- peloton/__init__.py
+++ peloton/__init__.py
@@ -2,10 +2,9 @@ from peloton.constants import (DATA_DIR, EASTERN_TIME, IMAGES_DIR,
                                INSTRUCTORS_JSON, MARIADB_DATABASE,
                                PELOTON_BASE_URL, PELOTON_SPREADSHEET,
                                SESSION_JSON, SQLITE_FILENAME, WORKOUTS_DIR)
-from peloton.handlers import (PelotonChartMaker, PelotonCSVWriter,
+from peloton.handlers import (PelotonChartMaker, PylotonZMV,
                               PelotonPivots, PelotonJSONWriter,
-                              PelotonMongoDB, PelotonSQL, PelotonStdoutPrinter, 
-                              PylotonZMV)
+                              PelotonMongoDB, PelotonSQL)
 from peloton.helpers import create_mariadb_engine
 from peloton.peloton_processor import PelotonProcessor
 
