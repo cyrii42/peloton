a4e706e4c253b6eba8e3f08fc4bf1c69fb863eb2
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-06 23:24:14 -0400

    moved `json_handler` to trash; cleaned up imports

--- peloton/handlers/__init__.py
+++ peloton/handlers/__init__.py
@@ -1,5 +1,4 @@
 from .sql_handler import PelotonSQL
-from .json_handler import PelotonJSONWriter
 from .pivot_table_maker import PelotonPivots
 from .chart_maker import PelotonChartMaker
 from .mongodb_handler import PelotonMongoDB
