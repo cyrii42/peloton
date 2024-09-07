ef783c63cfdb0ebb1f219e5fb954aa62b6e33d33
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-06 23:24:53 -0400

    renamed MongoDB methods to generic names (for dependency injection)

--- peloton/handlers/mongodb_handler.py
+++ peloton/handlers/mongodb_handler.py
@@ -10,7 +10,7 @@ class PelotonMongoDB():
     def __init__(self):
         self.mongodb_client = MongoDBConnection()
 
-    def get_workout_id_list_from_mongodb(self) -> list[str]:
+    def get_workout_id_list(self) -> list[str]:
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
             collection = db[MONGODB_COLLECTION]
@@ -19,7 +19,7 @@ class PelotonMongoDB():
 
         return workout_id_list
 
-    def ingest_workouts_from_mongodb(self) -> list[PelotonWorkoutData]:
+    def ingest_workouts(self) -> list[PelotonWorkoutData]:
         print("Getting workouts from MongoDB...")
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
@@ -29,14 +29,14 @@ class PelotonMongoDB():
         
         return workout_list
 
-    def export_workout_to_mongodb(self, workout: PelotonWorkoutData) -> None:
+    def export_workout(self, workout: PelotonWorkoutData) -> None:
         print(f"Exporting workout {workout.workout_id} to MongoDB...")
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
             collection = db[MONGODB_COLLECTION]
             collection.insert_one(workout.model_dump())
         
-    def update_workout_in_mongodb(self, workout: PelotonWorkoutData) -> None:
+    def update_workout(self, workout: PelotonWorkoutData) -> None:
         print(f"Updating workout {workout.workout_id} on MongoDB with new data...")
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
@@ -45,32 +45,21 @@ class PelotonMongoDB():
                 filter={'workout_id': workout.workout_id},
                 replacement=workout.model_dump())
 
-    def get_workout_from_mongodb(self, workout_id: str) -> PelotonWorkoutData:
+    def get_workout(self, workout_id: str) -> PelotonWorkoutData:
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
             collection = db[MONGODB_COLLECTION]
             workout = collection.find_one({'workout_id': workout_id})
         return PelotonWorkoutData(**workout)
 
-    def write_workout_to_json(self, workout: PelotonWorkoutData) -> None:
-        print(f"Writing JSON file for workout {workout.workout_id} to disk...")
-        with open(WORKOUTS_DIR.joinpath(f"{workout.workout_id}.json"), 'w') as f:
-            json.dump(workout.model_dump(), f, indent=4)
-
-    def backup_all_workouts_to_json(self) -> None:
-        workouts = self.ingest_workouts_from_mongodb()
-        for workout in workouts:
-            print(f"Writing workout {workout.workout_id} to JSON...")
-            self.write_workout_to_json(workout)
-
-    def get_instructor_info_from_instructor_id(self, instructor_id: str) -> dict:
+    def get_instructor(self, instructor_id: str) -> dict:
         with self.mongodb_client as client: 
             db = client.connection[MONGODB_DATABASE]
             collection = db[MONGODB_INSTRUCTORS_COLLECTION]
             instructor = collection.find_one({'instructor_id': instructor_id})
         return instructor
 
-    def add_instructor_to_mongodb(self, instructor: dict) -> None:
+    def add_instructor(self, instructor: dict) -> None:
         with self.mongodb_client as client:
             db = client.connection[MONGODB_DATABASE]
             collection = db[MONGODB_INSTRUCTORS_COLLECTION]
