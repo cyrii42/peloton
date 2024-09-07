ac294ee3de252b36e1d6403d7c4193a7d5431026
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-06 21:31:29 -0400

    deleted `image_downloader.py` and removed import from handlers init file

--- peloton/handlers/__init__.py
+++ peloton/handlers/__init__.py
@@ -6,4 +6,3 @@ from .stdout_printer import PelotonStdoutPrinter
 from .chart_maker import PelotonChartMaker
 from .mongodb_handler import PelotonMongoDB
 from .mongodb_conn import MongoDBConnection
-from .image_downloader import PelotonImageDownloader
