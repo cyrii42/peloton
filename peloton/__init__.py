ad9e8e194646fed0ea2230c38e60ca971eb1bd87
Zach Vaughan <85461794+cyrii42@users.noreply.github.com>
2024-09-07 22:11:40 -0400

    revised imports - now only `PelotonProcessor` is directly imported

--- peloton/__init__.py
+++ peloton/__init__.py
@@ -1,13 +1,22 @@
-from peloton.helpers.constants import (DATA_DIR, EASTERN_TIME, IMAGES_DIR,
-                               INSTRUCTORS_JSON, MARIADB_DATABASE,
-                               PELOTON_BASE_URL, PELOTON_SPREADSHEET,
-                               SESSION_JSON, SQLITE_FILENAME, WORKOUTS_DIR)
+# from peloton.helpers.constants import (DATA_DIR, EASTERN_TIME, IMAGES_DIR,
+#                                INSTRUCTORS_JSON, MARIADB_DATABASE,
+#                                PELOTON_BASE_URL, PELOTON_SPREADSHEET,
+#                                SESSION_JSON, SQLITE_FILENAME, WORKOUTS_DIR)
 
-from peloton.handlers import (PelotonChartMaker, PylotonZMV,
-                              PelotonPivots, PelotonMongoDB,
-                              PelotonSQL, PelotonProcessor)
 
-from peloton.models import (PelotonHumanInstructor, PelotonMetrics,
-                            PelotonNonHumanInstructor, PelotonSummary,
-                            PelotonWorkoutData, PelotonDataFrameRow,
-                            PelotonPivotTableRow)
+
+# from peloton.handlers import (PelotonChartMaker, PylotonZMV,
+#                               PelotonPivots, PelotonMongoDB,
+#                               PelotonSQL, PelotonProcessor)
+
+# from peloton.models import (PelotonHumanInstructor, PelotonMetrics,
+#                             PelotonNonHumanInstructor, PelotonSummary,
+#                             PelotonWorkoutData, PelotonDataFrameRow,
+#                             PelotonPivotTableRow)
+
+import peloton.helpers.exceptions
+import peloton.helpers.constants
+import peloton.handlers
+import peloton.models
+
+from peloton.handlers import PelotonProcessor
